268a340645fcb9960e9bf37e0f06819da7caafb4
Jonathan <31160969+JBCarroll@users.noreply.github.com>
2021-05-06 15:45:34 +0200

    Add files via upload

--- classes/class-alter-form.php
+++ classes/class-alter-form.php
@@ -8,56 +8,97 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="alter_wrapper">
     <div class="form-row">
         <div class="col">
-            <label for="alter_province">WHERE:</label>
+            <label for="alter_province">Province:</label>
             <?php if( have_rows('service_provider_searches', 'option') ): ?>
-                    <select id="alter_province" class="province form-control">
-                    <option value="none" selected disabled>Select province</option>
+                    <select id="alter_province" class="alter-select province form-control">
+                    <option value="none" selected disabled>Select Province</option>
                 <?php while( have_rows('service_provider_searches', 'option') ): the_row(); ?>
-                        <option><?php the_sub_field('province'); ?></option>
+                        <option onclick="filterSelection('<?php the_sub_field('province'); ?>')"><?php the_sub_field('filter_province'); ?></option>
                 <?php endwhile; ?>
                     </select>
             <?php endif; ?>
         </div>
         <div class="col">
+            <label for="alter_city">City:</label>
             <?php if( have_rows('service_provider_searches', 'option') ): ?>
-                <select id="alter_city" class="city form-control">
-                    <option value="none" selected disabled>Select city</option>
+                <select id="alter_city" class="alter-select city form-control">
+                    <option value="none" selected disabled>Select City</option>
                 <?php while( have_rows('service_provider_searches', 'option') ): the_row(); ?>
-                    <option><?php the_sub_field('city'); ?></option>
+                    <option onclick="filterSelection('<?php the_sub_field('city'); ?>')"><?php the_sub_field('filter_city'); ?></option>
                 <?php endwhile; ?>
                 </select>
             <?php endif; ?>
         </div>
         <div class="col">
-            <label for="alter_store">STORE TYPE:</label>
+            <label for="alter_store">Category:</label>
             <?php if( have_rows('service_provider_searches', 'option') ): ?>
-                <select id="alter_store" class="store form-control">
-                    <option value="none" selected disabled>Select store</option>
+                <select id="alter_store" class="alter-select store form-control">
+                    <option value="none" selected>Select Category</option>
                 <?php while( have_rows('service_provider_searches', 'option') ): the_row(); ?>
-                    <option><?php the_sub_field('store_type'); ?></option>
+                    <option onclick="filterSelection('<?php the_sub_field('provider_category'); ?>')"><?php the_sub_field('filter_category'); ?></option>
                 <?php endwhile; ?>
                 </select>
             <?php endif; ?>
         </div>
         <!-- SEARCH INPUT -->
         <div class="col">
-            <input id="alter_search" type="text" class="search form-control" placeholder="Search here..." />
+            <span id="alter-reset">Reset</span>
         </div>
     </div>
         <span class="counter pull-right"></span>
-    <table class="table table-hover table-bordered results">
-        <tbody>
-        <h4>Nationwide retailers in: </h4>
-            <?php if( have_rows('service_provider_results', 'option') ): ?>
-                    <?php while( have_rows('service_provider_results', 'option') ): the_row(); ?>
-                    <tr class="warning no-result">
-                        <td colspan="4"><i class="fa fa-warning"></i> No result</td>
-                    </tr>
-                        <tr>
-                            <td class="ther_name"><img src="<?php the_sub_field('provider_image'); ?>"/></td>
-                        </tr>
-                    <?php endwhile; ?>
-            <?php endif; ?>
-        </tbody>
-    </table>
+    <div class="container">
+        <?php if( have_rows('service_provider_searches', 'option') ): ?>
+            <?php while( have_rows('service_provider_searches', 'option') ): the_row(); ?>
+                    <div class="filterDiv <?php the_sub_field('province'); ?><?php echo " " ?><?php the_sub_field('city'); ?><?php echo " " ?><?php the_sub_field('provider_category'); ?>">
+                    <a href="#" class="link">
+                        <img src="<?php the_sub_field('provider_image'); ?>"/>
+                        <span class="title"><?php the_sub_field('provider_name'); ?></span>
+                    </a>
+                    </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 </div>
+
+<script>
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+jQuery(function($){
+    $("#alter-reset").on("click", function () {
+        $('.alter-select option').prop('selected', function() {
+            return this.defaultSelected;
+        });
+    });
+});
+
+</script>
--- js/results.js
+++ js/results.js
@@ -1,31 +1,27 @@
-jQuery(document).ready(function($){
-    $("input").keyup(function(){
-        
-        var searchProvince = $(".province").val();
-        var searchCity = $(".city").val();
-        var searchStoreType = $(".store").val();
-        var searchTerm = $(".search").val();
+// jQuery(document).ready(function($){
+//     $("input").keyup(function(){
+//         var searchTerm = $(".search").val();
 
-        var listItem = $('.results tbody').children('tr');
-        var searchSplit = (searchTerm).replace(/ /g, "'):containsi('");
+//         var listItem = $('.results tbody').children('tr');
+//         var searchSplit = (searchTerm).replace(/ /g, "'):containsi('");
 
-        $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-            return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-        }
-        });
+//         $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+//             return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+//         }
+//         });
 
-        $(".results tbody tr").not(":containsi('" + searchSplit +  "')").each(function(e){
-        $(this).attr('visible','false');
-        });
+//         $(".results tbody tr").not(":containsi('" + searchSplit +  "')").each(function(e){
+//         $(this).attr('visible','false');
+//         });
     
-        $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-        $(this).attr('visible','true');
-        });
+//         $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+//         $(this).attr('visible','true');
+//         });
 
-        var jobCount = $('.results tbody tr[visible="true"]').length;
-        $('.counter').text('Results Found: ' + jobCount + ' Service Providers');
+//         var jobCount = $('.results tbody tr[visible="true"]').length;
+//         $('.counter').text('Results Found: ' + jobCount + ' Service Providers');
 
-        if(jobCount === '0') {$('.no-result').show();}
-        else {$('.no-result').hide();}
-    });
-});
+//         if(jobCount === '0') {$('.no-result').show();}
+//         else {$('.no-result').hide();}
+//     });
+// });
